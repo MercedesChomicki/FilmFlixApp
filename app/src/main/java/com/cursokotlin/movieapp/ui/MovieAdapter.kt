ace68a50dcac533353066ab6de7bf48d117ca6d0
Maria Mercedes Chomicki <mmchomicki1995@gmail.com>
2024-10-22 23:14:18 -0300

    Agregado de binding en MovieAdapter

--- app/src/main/java/com/cursokotlin/movieapp/ui/MovieAdapter.kt
+++ app/src/main/java/com/cursokotlin/movieapp/ui/MovieAdapter.kt
@@ -1,35 +1,30 @@
 package com.cursokotlin.movieapp.ui
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.cursokotlin.movieapp.BuildConfig
-import com.cursokotlin.movieapp.R
+import com.cursokotlin.movieapp.databinding.ItemMovieBinding
 import com.cursokotlin.movieapp.ddl.models.Movie
 
 class MovieAdapter(
     private val movies: List<Movie>
 ) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
-    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val title = itemView.findViewById<TextView>(R.id.movieTitle)
-        private val poster = itemView.findViewById<ImageView>(R.id.moviePoster)
+    class MovieViewHolder(private var binding : ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(movie: Movie) {
-            title.text = movie.title
-            Glide.with(itemView.context)
+            binding.movieTitle.text = movie.title
+            Glide.with(binding.root.context)
                 .load(BuildConfig.BASE_URL_IMAGES + movie.poster_path)
-                .into(poster)
+                .into(binding.moviePoster)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
-        return MovieViewHolder(view)
+        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
